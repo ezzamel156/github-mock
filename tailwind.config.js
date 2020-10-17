cfd75b64cf8d4c4a6c8a6e162a94122423fca098
ezzamel <ezzamel156@gmail.com>
2020-10-17 18:48:21 +0800

    Installed tailwind typography and made page slightly responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,34 @@ module.exports = {
   },
   purge: [],
   theme: {
+    typography: (theme) => ({
+      default: {
+        css: {
+          a: {
+            color: theme('colors.blue.600'),
+            textDecoration: 'none',
+            '&:hover': {
+              textDecoration: 'underline',
+            },
+          },
+          img: {
+            display: 'inline-block',
+          },
+          hr: {
+            borderColor: theme('colors.gray.400'),
+            marginTop: '2em',
+            marginBottom: '2em',
+          }
+
+        },
+      },
+    }),
     extend: {},
   },
   variants: {
     textColor: ({ after }) => after(['group-hover']),
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
